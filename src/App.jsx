de2df12aac7e5d41818c84ecff07c97d47d596c7
Asmit Aditya Singh <asmitadsingh13@gamil.com>
2026-02-26 17:32:34 +0530

    Code Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import List from "./pages/List";
 import Details from "./pages/Details";
 import PhotoResult from "./pages/PhotoResult";
